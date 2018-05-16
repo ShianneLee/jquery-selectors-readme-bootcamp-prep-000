f6958504ebb520d017d85e10d2d51958f8bc1e68
Shianne Lee <shiannelee@live.ca>
2018-05-16 03:46:28 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,6 +1,6 @@
 // declare your functions here...
 function paragraphSelector(){ return $('p')};
 function lastImageSelector(){ return $('img:last')};
-function ninjaBabyhSelector(){ return $('#baby-ninja')};
+function ninjaBabySelector(){ return $('#baby-ninja')};
 function divSelector(){ return $('.pics')};
-function firstListItem(){ return $('ul pic-list:first-child')};
+function firstListItem(){ return $('ul #pic-list:first-child')};
